3f8d6d574c5a30093dae1b436ba4d98ad0d75da9
Admadma <aranyiadam@gmail.com>
2023-04-16 13:23:37 +0200

    Invalidating sesion if user becomes invalid while reserving room

--- hotel-booking-app-web/src/main/java/com/application/hotelbooking/controllers/ReservationController.java
+++ hotel-booking-app-web/src/main/java/com/application/hotelbooking/controllers/ReservationController.java
@@ -5,6 +5,7 @@ import com.application.hotelbooking.exceptions.InvalidUserException;
 import com.application.hotelbooking.exceptions.NoRoomsAvailableException;
 import com.application.hotelbooking.services.ReservationService;
 import com.application.hotelbooking.services.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,15 +29,20 @@ public class ReservationController {
     private UserService userService;
 
     @RequestMapping(value = "/select-room-type")
-    public String reserveRoom(@ModelAttribute("roomType") String roomType, Authentication auth){
+    public String reserveRoom(@ModelAttribute("roomType") String roomType, Authentication auth, HttpSession http){
         // TODO: later surround this with a try-catch. Exceptions can be for example: time period is invalid
         // TODO: user can edit the html code and change the room id. Code needs to check that there was no tampering with the parameters (e.g. clicking book this room but the id was changed to a different room)
+
+        // auth -> some_validator -> checks whether there is exactly one instance of that user in the database. -> if false -> invalidate http and redirect to login page
+
         try {
             Reservation reservation =reservationService.reserve(roomType, auth.getName(), LocalDate.now(), LocalDate.now().plusDays(7));
             LOGGER.info("Successfully created reservation: " + reservation.getId() + " reserved room: " + reservation.getRoom().getId() + " user: " + reservation.getUser().getUsername());
         } catch (InvalidUserException iue){
-            // TODO: if this error occurs, maybe I should log out the user
             LOGGER.error(iue.getMessage());
+            http.invalidate();
+            // After invalidating, I don't want to reach the rest of this method
+            return "redirect:/login";
         } catch (NoRoomsAvailableException nae) {
             LOGGER.error("Could not make a reservation because there are no free rooms of that type");
         }
