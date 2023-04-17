9df416e0b790d39c5316617753c0b46a8301a6f7
Admadma <aranyiadam@gmail.com>
2023-04-17 20:58:31 +0200

    Handling registration errors

--- hotel-booking-app-web/src/main/java/com/application/hotelbooking/dto/UserDto.java
+++ hotel-booking-app-web/src/main/java/com/application/hotelbooking/dto/UserDto.java
@@ -1,6 +1,7 @@
 package com.application.hotelbooking.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,13 +13,6 @@ public class UserDto {
     @NotEmpty(message = "Please enter a username")
     private String username;
     @NotEmpty(message = "Please enter a password")
+    @Size(min = 8, max = 25)
     private String password;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
