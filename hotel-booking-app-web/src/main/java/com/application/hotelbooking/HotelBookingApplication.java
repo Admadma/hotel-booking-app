6db7ffe400a8ca9c2965e72bbedf9bc76c74f140
Admadma <aranyiadam@gmail.com>
2023-04-17 22:56:07 +0200

    Added localization. Language selection still needs refactoring

--- hotel-booking-app-web/src/main/java/com/application/hotelbooking/HotelBookingApplication.java
+++ hotel-booking-app-web/src/main/java/com/application/hotelbooking/HotelBookingApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class HotelBookingApplication {
@@ -14,6 +19,8 @@ public class HotelBookingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HotelBookingApplication.class, args);
 	}
+
+
 
 ////	//Code used to generate demo data
 //	@Bean
--- hotel-booking-app-web/src/main/java/com/application/hotelbooking/controllers/HomeController.java
+++ hotel-booking-app-web/src/main/java/com/application/hotelbooking/controllers/HomeController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+import java.util.Locale;
+
 @Controller
 @RequestMapping(path = "hotelbooking")
 public class HomeController {
