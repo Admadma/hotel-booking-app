06d33a4caa8dde36c5ae7c81f8a9fc02b228e5d8
Admadma <aranyiadam@gmail.com>
2023-04-19 20:08:20 +0200

    Added new room types

---
+++ hotel-booking-app-domain/src/main/java/com/application/hotelbooking/domain/TwinRoom.java
@@ -0,0 +1,15 @@
+package com.application.hotelbooking.domain;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@DiscriminatorValue("twinRoom")
+public class TwinRoom {
+}
