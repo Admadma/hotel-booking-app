071096565b4942093478a7d1acf0aa917f775156
Admadma <aranyiadam@gmail.com>
2023-04-29 23:06:27 +0200

    Displaying attributes of reservation to user before reserving

--- hotel-booking-app-web/src/main/java/com/application/hotelbooking/controllers/ConfirmReservationController.java
+++ hotel-booking-app-web/src/main/java/com/application/hotelbooking/controllers/ConfirmReservationController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
@@ -41,8 +42,10 @@ public class ConfirmReservationController {
 
 
     @GetMapping("/confirmreservation")
-    public String confirmReservation(){
+    public String confirmReservation(@SessionAttribute("reservation") ReservationView reservationView, Model model){
         LOGGER.info("Navigating to confirmreservation page");
+
+        model.addAttribute("reservation", reservationView);
 
         return "confirmreservation";
     }
