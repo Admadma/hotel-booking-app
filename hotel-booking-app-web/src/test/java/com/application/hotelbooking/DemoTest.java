7f5a17c422282529edadcbf8ebcdcd67994071f5
Admadma <admadmaa77@gmail.com>
2023-10-21 22:42:21 +0200

    Added Jacoco dependency for generating code report and coverage check

---
+++ hotel-booking-app-web/src/test/java/com/application/hotelbooking/DemoTest.java
@@ -0,0 +1,12 @@
+package com.application.hotelbooking;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+
+    @Test
+    public void testDemoMethod(){
+        Assertions.assertTrue(true);
+    }
+}
