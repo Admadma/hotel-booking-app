b18676eb9a61ed9d31d019de86e43c6dc11a3b6a
Admadma <admadmaa77@gmail.com>
2023-11-01 01:10:22 +0100

    Added quick bugfix to ModelMapper

--- hotel-booking-app-service/src/main/java/com/application/hotelbooking/transformers/RoomCreationServiceDTOTransformer.java
+++ hotel-booking-app-service/src/main/java/com/application/hotelbooking/transformers/RoomCreationServiceDTOTransformer.java
@@ -1,7 +1,6 @@
 package com.application.hotelbooking.transformers;
 
 import com.application.hotelbooking.domain.Room;
-import com.application.hotelbooking.domain.RoomModel;
 import com.application.hotelbooking.dto.RoomCreationServiceDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +13,7 @@ public class RoomCreationServiceDTOTransformer {
     private ModelMapper modelMapper;
 
     public Room transformToRoom(RoomCreationServiceDTO roomCreationServiceDTO){
+        modelMapper.typeMap(RoomCreationServiceDTO.class, Room.class).addMappings(modelMapper -> modelMapper.skip(Room::setId));
         return modelMapper.map(roomCreationServiceDTO, Room.class);
     }
 }
