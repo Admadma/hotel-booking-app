37afd85e5762011b55a7f4253542e6e8ddd5d339
Admadma <admadmaa77@gmail.com>
2023-11-13 00:10:20 +0100

    Updated entity id GenerationType to IDENTITY

--- hotel-booking-app-domain/src/main/java/com/application/hotelbooking/domain/User.java
+++ hotel-booking-app-domain/src/main/java/com/application/hotelbooking/domain/User.java
@@ -17,7 +17,7 @@ import java.util.Objects;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long version;
