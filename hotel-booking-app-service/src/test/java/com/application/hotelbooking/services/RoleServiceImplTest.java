58eafdc4af2403870ba2e8a5f9c44f05473be10a
Admadma <admadmaa77@gmail.com>
2023-11-21 23:08:54 +0100

    Fixed test for checking getRoles and getRolesSimpler behavious. The getRolesSimpler method can now take the place of getRoles.

--- hotel-booking-app-service/src/test/java/com/application/hotelbooking/services/RoleServiceImplTest.java
+++ hotel-booking-app-service/src/test/java/com/application/hotelbooking/services/RoleServiceImplTest.java
@@ -23,9 +23,10 @@ public class RoleServiceImplTest {
 
     public static final String TEST_ROLE = "TestRole";
     public static final RoleModel TEST_ROLE_MODEL = RoleModel.builder().name(TEST_ROLE).build();
-    public static final List<String> ROLE_NAMES = List.of("NAME2");
-    public static final List<Role> ROLES = List.of(Role.builder().name("NAME1").build(), Role.builder().name("NAME2").build());
-    public static final List<RoleModel> ROLE_MODELS = List.of(RoleModel.builder().name("NAME1").build(), RoleModel.builder().name("NAME2").build());
+    public static final List<String> ROLE_NAMES = List.of("NAME1", "NAME3");
+    public static final List<Role> ROLES = List.of(Role.builder().name("NAME1").build(), Role.builder().name("NAME2").build(), Role.builder().name("NAME3").build());
+    public static final List<Role> SELECTED_ROLES = List.of(Role.builder().name("NAME1").build(), Role.builder().name("NAME3").build());
+    public static final List<RoleModel> SELECTED_ROLE_MODELS = List.of(RoleModel.builder().name("NAME1").build(), RoleModel.builder().name("NAME3").build());
     @InjectMocks
     private RoleServiceImpl roleService;
 
@@ -44,26 +45,26 @@ public class RoleServiceImplTest {
     @Test
     public void testGetRolesShouldReturnCollectionOfRolesWhereNameIsInTheList(){
         when(roleRepository.findAll()).thenReturn(ROLES);
-        when(roleTransformer.transformToRoleModels(ROLES)).thenReturn(ROLE_MODELS);
+        when(roleTransformer.transformToRoleModels(SELECTED_ROLES)).thenReturn(SELECTED_ROLE_MODELS);
 
         Collection<RoleModel> roleModels = roleService.getRoles(ROLE_NAMES);
 
         verify(roleRepository).findAll();
-        verify(roleTransformer).transformToRoleModels(ROLES);
+        verify(roleTransformer).transformToRoleModels(SELECTED_ROLES);
         Assertions.assertThat(roleModels).isNotNull();
-        Assertions.assertThat(roleModels.size()).isEqualTo(1);
+        Assertions.assertThat(roleModels).isEqualTo(SELECTED_ROLE_MODELS);
     }
 
     @Test
     public void testGetRolesSimplerShouldReturnCollectionOfRolesWhereNameIsInTheList(){
-        when(roleRepository.findByNameIn(ROLE_NAMES)).thenReturn(ROLES);
-        when(roleTransformer.transformToRoleModels(ROLES)).thenReturn(ROLE_MODELS);
+        when(roleRepository.findByNameIn(ROLE_NAMES)).thenReturn(SELECTED_ROLES);
+        when(roleTransformer.transformToRoleModels(SELECTED_ROLES)).thenReturn(SELECTED_ROLE_MODELS);
 
         Collection<RoleModel> roleModels = roleService.getRolesSimpler(ROLE_NAMES);
 
         verify(roleRepository).findByNameIn(ROLE_NAMES);
-        verify(roleTransformer).transformToRoleModels(ROLES);
+        verify(roleTransformer).transformToRoleModels(SELECTED_ROLES);
         Assertions.assertThat(roleModels).isNotNull();
-        Assertions.assertThat(roleModels.size()).isEqualTo(1);
+        Assertions.assertThat(roleModels).isEqualTo(SELECTED_ROLE_MODELS);
     }
 }
