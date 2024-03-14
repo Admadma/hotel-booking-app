38a9bceed442e68378398231f56fc07b3fbd9d33
Admadma <admadmaa77@gmail.com>
2024-03-14 22:39:53 +0100

    Added test for post mapping controller method with request parameter

--- hotel-booking-app-web/src/test/java/com/application/hotelbooking/controllers/AddHotelsControllerTest.java
+++ hotel-booking-app-web/src/test/java/com/application/hotelbooking/controllers/AddHotelsControllerTest.java
@@ -11,10 +11,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AddHotelsController.class)
 public class AddHotelsControllerTest {
+
+    private HotelCreationDTO HOTEL_SHORT_NAME_HAS_CITY = new HotelCreationDTO("A", "City name");
+    private HotelCreationDTO HOTEL_GOOD_NAME_NO_CITY = new HotelCreationDTO("Long name", "");
 
     @MockBean
     private HotelService hotelService;
@@ -26,12 +31,36 @@ public class AddHotelsControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    @WithMockUser(username = "testUser", roles = {"USER", "ADMIN"})
+    @WithMockUser(username = "testUser", roles = "ADMIN")
     public void testAdminUserCanNavigateToAddHotelsPage() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/hotelbooking/admin/addHotels"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("addhotels"))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("hotelCreationDTO"))
-                .andExpect(MockMvcResultMatchers.model().attribute("hotelCreationDTO", Matchers.any(HotelCreationDTO.class)));
+                .andExpect(status().isOk())
+                .andExpect(view().name("addhotels"))
+                .andExpect(model().attributeExists("hotelCreationDTO"))
+                .andExpect(model().attribute("hotelCreationDTO", Matchers.any(HotelCreationDTO.class)));
+    }
+
+    @Test
+    @WithMockUser(username = "testUser", roles = "ADMIN")
+    public void testCreateNewHotelShouldReturnToAddHotelsPageWithErrorIfProvidedAttributesAreInvalid() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/hotelbooking/admin/create-new-hotel")
+                        .flashAttr("hotelCreationDTO", HOTEL_SHORT_NAME_HAS_CITY)
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("addhotels"))
+                .andExpect(model().attributeExists("hotelCreationDTO"))
+                .andExpect(model().attributeHasErrors("hotelCreationDTO"))
+                .andExpect(model().attribute("hotelCreationDTO", Matchers.any(HotelCreationDTO.class)));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/hotelbooking/admin/create-new-hotel")
+                        .flashAttr("hotelCreationDTO", HOTEL_GOOD_NAME_NO_CITY)
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("addhotels"))
+                .andExpect(model().attributeExists("hotelCreationDTO"))
+                .andExpect(model().attributeHasErrors("hotelCreationDTO"))
+                .andExpect(model().attribute("hotelCreationDTO", Matchers.any(HotelCreationDTO.class)));
     }
 }
---
+++ hotel-booking-app-web/src/test/java/com/application/hotelbooking/controllers/ConfirmEmailControllerTest.java
@@ -0,0 +1,22 @@
+package com.application.hotelbooking.controllers;
+
+import com.application.hotelbooking.services.ResendConfirmationTokenService;
+import com.application.hotelbooking.services.UserEmailConfirmationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(ConfirmEmailController.class)
+public class ConfirmEmailControllerTest {
+
+    @MockBean
+    private UserEmailConfirmationService userEmailConfirmationService;
+
+    @MockBean
+    private ResendConfirmationTokenService resendConfirmationTokenService;
+
+    @Autowired
+    private MockMvc mockMvc;
+}
