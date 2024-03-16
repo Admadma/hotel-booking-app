a2fa3d7536c10f18964ef7fa113ecc5910731432
Admadma <admadmaa77@gmail.com>
2024-03-16 22:07:07 +0100

    Added tests for MyUserDetails

---
+++ hotel-booking-app-web/src/test/java/com/application/hotelbooking/security/MyUserDetailsTest.java
@@ -0,0 +1,95 @@
+package com.application.hotelbooking.security;
+
+import com.application.hotelbooking.domain.RoleView;
+import com.application.hotelbooking.domain.UserView;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class MyUserDetailsTest {
+
+    public static final RoleView ROLE_VIEW = new RoleView(1L, "USER", null);
+    public static final List<RoleView> ROLE_VIEW_LIST = List.of(ROLE_VIEW);
+    public static final Collection<GrantedAuthority> GRANTED_AUTHORITIES = List.of(new SimpleGrantedAuthority("USER"));
+    @InjectMocks
+    private MyUserDetails myUserDetails;
+
+    @Mock
+    private UserView userView;
+
+    @Test
+    public void testGetAuthoritiesShouldReturnCollectionOfGrantedAuthoritiesBelongingToUser(){
+        when(userView.getRoles()).thenReturn(ROLE_VIEW_LIST);
+
+        Collection<? extends GrantedAuthority> resultGrantedAuthorities = myUserDetails.getAuthorities();
+
+        verify(userView).getRoles();
+        Assertions.assertThat(resultGrantedAuthorities).isEqualTo(GRANTED_AUTHORITIES);
+    }
+
+    @Test
+    public void testGetPasswordShouldReturnPasswordOfUser(){
+        when(userView.getPassword()).thenReturn("asd");
+
+        String resultPassword = myUserDetails.getPassword();
+
+        verify(userView).getPassword();
+        Assertions.assertThat(resultPassword).isEqualTo("asd");
+    }
+
+    @Test
+    public void testGetUsernameShouldReturnUsernameOfUser(){
+        when(userView.getUsername()).thenReturn("asd");
+
+        String resultUsername = myUserDetails.getUsername();
+
+        verify(userView).getUsername();
+        Assertions.assertThat(resultUsername).isEqualTo("asd");
+    }
+
+    @Test
+    public void testIsAccountNonExpiredShouldReturnTrue(){
+        boolean resultIsAccountNonExpired = myUserDetails.isAccountNonExpired();
+
+        Assertions.assertThat(resultIsAccountNonExpired).isEqualTo(true);
+    }
+
+    @Test
+    public void testIsAccountNonLockedShouldReturnTrueIfAccountIsNotLocked(){
+        when(userView.getLocked()).thenReturn(false);
+
+        boolean resultIsAccountNonLocked = myUserDetails.isAccountNonLocked();
+
+        verify(userView).getLocked();
+        Assertions.assertThat(resultIsAccountNonLocked).isEqualTo(true);
+    }
+
+    @Test
+    public void testIsCredentialsNonExpiredShouldReturnTrue(){
+        boolean resultIsCredentialsNonExpired = myUserDetails.isCredentialsNonExpired();
+
+        Assertions.assertThat(resultIsCredentialsNonExpired).isEqualTo(true);
+    }
+
+    @Test
+    public void testIsEnabledShouldReturnTrueIfAccountIsEnabled(){
+        when(userView.getEnabled()).thenReturn(true);
+
+        boolean resultIsAccountNonLocked = myUserDetails.isEnabled();
+
+        verify(userView).getEnabled();
+        Assertions.assertThat(resultIsAccountNonLocked).isEqualTo(true);
+    }
+}
