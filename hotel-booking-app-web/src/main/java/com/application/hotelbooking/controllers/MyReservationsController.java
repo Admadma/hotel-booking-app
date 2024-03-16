237c77de72b64504fb43b86b819173b97131f4ea
Admadma <admadmaa77@gmail.com>
2024-03-16 19:54:24 +0100

    Added tests for MyReservationsController

--- hotel-booking-app-web/src/main/java/com/application/hotelbooking/controllers/MyReservationsController.java
+++ hotel-booking-app-web/src/main/java/com/application/hotelbooking/controllers/MyReservationsController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -27,7 +28,7 @@ public class MyReservationsController {
     @Autowired
     private ReservationViewTransformer reservationViewTransformer;
 
-    @RequestMapping(value = "/cancel-reservation")
+    @PostMapping(value = "/cancel-reservation")
     public String cancelReservation(@ModelAttribute("reservationId") Long reservationId){
         try {
             reservationService.cancelReservation(reservationId);
--- hotel-booking-app-web/src/test/java/com/application/hotelbooking/controllers/MyReservationsControllerTest.java
+++ hotel-booking-app-web/src/test/java/com/application/hotelbooking/controllers/MyReservationsControllerTest.java
@@ -1,21 +1,51 @@
 package com.application.hotelbooking.controllers;
 
+import com.application.hotelbooking.domain.*;
+import com.application.hotelbooking.security.SecurityConfiguration;
 import com.application.hotelbooking.services.ReservationService;
 import com.application.hotelbooking.transformers.ReservationViewTransformer;
-import org.junit.jupiter.api.BeforeEach;
+import jakarta.persistence.OptimisticLockException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import java.time.LocalDate;
+import java.util.List;
 
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+@Import(SecurityConfiguration.class)
 @WebMvcTest(MyReservationsController.class)
 public class MyReservationsControllerTest {
+
+    private static final Long RESERVATION_ID = 1L;
+    private static final String TEST_USER_NAME = "test_user";
+    private static final ReservationModel RESERVATION_MODEL = new ReservationModel();
+    private static final List<ReservationModel> RESERVATION_MODEL_LIST = List.of(RESERVATION_MODEL);
+    private static final HotelView HOTEL_VIEW = HotelView.builder()
+            .hotelName("Test Hotel")
+            .city("Test City")
+            .build();
+    private static final RoomView ROOM_VIEW = RoomView.builder()
+            .roomNumber(1)
+            .hotel(HOTEL_VIEW)
+            .roomType(RoomType.FAMILY_ROOM)
+            .build();
+    private static final ReservationView RESERVATION_VIEW = ReservationView.builder()
+            .room(ROOM_VIEW)
+            .startDate(LocalDate.now().plusDays(5))
+            .endDate(LocalDate.now().plusDays(10))
+            .totalPrice(100)
+            .build();
+    private static final List<ReservationView> RESERVATION_VIEW_LIST = List.of(RESERVATION_VIEW);
 
     @MockBean
     private ReservationService reservationService;
@@ -25,22 +55,58 @@ public class MyReservationsControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-    /*
-    private MockMvc mockMvc;
-
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @BeforeEach
-    void setupMockMvc() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(springSecurity())
-                .build();
-    }
-    */
 
     @Test
-    void shouldCreateMockMVC(){
-        assertNotNull(mockMvc);
+    @WithMockUser(username = TEST_USER_NAME, authorities = "USER")
+    public void testUserCanNavigateToMyReservationsPage() throws Exception {
+        when(reservationService.getReservationsOfUser(TEST_USER_NAME)).thenReturn(RESERVATION_MODEL_LIST);
+        when(reservationViewTransformer.transformToReservationViews(RESERVATION_MODEL_LIST)).thenReturn(RESERVATION_VIEW_LIST);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/hotelbooking/myreservations"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("myreservations"))
+                .andExpect(model().attribute("reservations", RESERVATION_VIEW_LIST));
+
+        verify(reservationService).getReservationsOfUser(TEST_USER_NAME);
+        verify(reservationViewTransformer).transformToReservationViews(RESERVATION_MODEL_LIST);
+    }
+
+    @Test
+    @WithMockUser(username = TEST_USER_NAME, authorities = "ADMIN")
+    public void testAdminUserCanNotNavigateToMyReservationsPage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hotelbooking/myreservations"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username = TEST_USER_NAME, authorities = "ADMIN")
+    public void testAdminUserCanNotAttemptToCancelOwnReservation() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/hotelbooking/cancel-reservation")
+                .flashAttr("reservationId", RESERVATION_ID))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(authorities = "USER")
+    public void testCancelReservationShouldRedirectToMyReservationsPageWithGenericErrorIfCancelReservationThrowsAnyException() throws Exception {
+        doThrow(DataIntegrityViolationException.class).when(reservationService).cancelReservation(RESERVATION_ID);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/hotelbooking/cancel-reservation")
+                        .flashAttr("reservationId", RESERVATION_ID))
+                .andExpect(redirectedUrl("/hotelbooking/myreservations?error"));
+
+        verify(reservationService).cancelReservation(RESERVATION_ID);
+    }
+
+    @Test
+    @WithMockUser(authorities = "USER")
+    public void testCancelReservationShouldRedirectToMyReservationsPageIfNoExceptionOccurredWhileCancellingReservation() throws Exception {
+        doNothing().when(reservationService).cancelReservation(RESERVATION_ID);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/hotelbooking/cancel-reservation")
+                        .flashAttr("reservationId", RESERVATION_ID))
+                .andExpect(redirectedUrl("/hotelbooking/myreservations"));
+
+        verify(reservationService).cancelReservation(RESERVATION_ID);
     }
 }
