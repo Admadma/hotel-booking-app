8ca4bb89401ad417731cfd93c19fdcb13482a9a9
Admadma <admadmaa77@gmail.com>
2024-03-16 14:10:31 +0100

    Added tests for AdminPageController

--- hotel-booking-app-web/src/main/java/com/application/hotelbooking/controllers/AdminPageController.java
+++ hotel-booking-app-web/src/main/java/com/application/hotelbooking/controllers/AdminPageController.java
@@ -1,14 +1,9 @@
 package com.application.hotelbooking.controllers;
 
-import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
