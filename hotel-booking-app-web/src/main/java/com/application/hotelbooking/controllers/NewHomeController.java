4122f4a3a53061b04b591aa1474b8d5708c505b1
Admadma <admadmaa77@gmail.com>
2024-04-05 21:59:14 +0200

    Added message for no matching rooms found

--- hotel-booking-app-web/src/main/java/com/application/hotelbooking/controllers/NewHomeController.java
+++ hotel-booking-app-web/src/main/java/com/application/hotelbooking/controllers/NewHomeController.java
@@ -2,7 +2,6 @@ package com.application.hotelbooking.controllers;
 
 import com.application.hotelbooking.domain.RoomType;
 import com.application.hotelbooking.dto.HotelWithReservableRoomsDTO;
-import com.application.hotelbooking.dto.HotelWithReservableRoomsServiceDTO;
 import com.application.hotelbooking.dto.RoomSearchFormDTO;
 import com.application.hotelbooking.services.RoomService;
 import com.application.hotelbooking.services.repositoryservices.HotelRepositoryService;
@@ -26,6 +25,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Controller
@@ -75,15 +75,16 @@ public class NewHomeController {
         }
 
         transformFieldsToNulls(roomSearchFormDTO);
-
-//        List<ReservableRoomViewDTO> resultDTOS = roomSearchDTOTransformer.transformToRoomSearchResultViewDTOs(roomService.searchRooms(roomSearchDTOTransformer.transformToRoomSearchFormServiceDTO(roomSearchFormDTO)));
         List<HotelWithReservableRoomsDTO> hotelsWithReservableRoomsDTOS = hotelsWithReservableRoomsDTOTransformer.transformToHotelsWithReservableRoomsDTOs(roomService.searchHotelsWithReservableRooms(roomSearchDTOTransformer.transformToRoomSearchFormServiceDTO(roomSearchFormDTO)));
 
-        request.getSession().setAttribute("resultDTOS", hotelsWithReservableRoomsDTOS);
-        LOGGER.info("result: " + hotelsWithReservableRoomsDTOS.toString());
+        if (hotelsWithReservableRoomsDTOS.size() > 0) {
+            request.getSession().setAttribute("resultDTOS", hotelsWithReservableRoomsDTOS);
+            request.getSession().removeAttribute("noMatchingResults");
+            return "redirect:/hotelbooking/newHome";
+        }
 
-//        LOGGER.info("No Error");
-//        redirectAttributes.addFlashAttribute("successRoomSearchFormDTO", roomSearchFormDTO);
+        request.getSession().removeAttribute("resultDTOS");
+        request.getSession().setAttribute("noMatchingResults", true);
         return "redirect:/hotelbooking/newHome";
     }
 
@@ -92,21 +93,19 @@ public class NewHomeController {
     public String homeWithParams(Model model, @ModelAttribute("successRoomSearchFormDTO") RoomSearchFormDTO roomSearchFormDTO, HttpServletRequest request){
         LOGGER.info("Navigating to home page");
 
-//        String hotelName = "Hotel_1";
-//        String city = "City_1";
-//        UniqueReservableRoomOfHotelDTO uniqueReservableRoomOfHotelDTO = UniqueReservableRoomOfHotelDTO.builder().singleBeds(1).build();
-//        UniqueReservableRoomOfHotelDTO uniqueReservableRoomOfHotelDTO2 = UniqueReservableRoomOfHotelDTO.builder().singleBeds(3).build();
-//
-////        List<HotelsWithReservableRoomsDTO> hotelsWithReservableRoomsDTOS =roomService.searchHotelsWithReservableRooms();
-//
-//        List<HotelsWithReservableRoomsDTO> hotelsWithReservableRoomsDTOS = List.of(new HotelsWithReservableRoomsDTO(hotelName, city, List.of(uniqueReservableRoomOfHotelDTO)), new HotelsWithReservableRoomsDTO(hotelName, city, List.of(uniqueReservableRoomOfHotelDTO, uniqueReservableRoomOfHotelDTO2)));
-//        request.getSession().setAttribute("hotelsWithReservableRoomsDTOS", hotelsWithReservableRoomsDTOS);
-
         model.addAttribute("roomSearchFormDTO", roomSearchFormDTO);
 
         request.getSession().setAttribute("roomTypes", Arrays.stream(RoomType.values()).map(roomType -> roomType.name()).collect(Collectors.toList()));
         request.getSession().setAttribute("hotels", hotelViewTransformer.transformToHotelViews(hotelRepositoryService.getAllHotels()));
         request.getSession().setAttribute("cities", hotelRepositoryService.getAllHotels().stream().map(hotelModel -> hotelModel.getCity()).collect(Collectors.toList()));
+
+        // List all the different rooms to the user within a set date interval
+        if (Objects.isNull(request.getSession().getAttribute("resultDTOS"))) {
+            roomSearchFormDTO.setStartDate(LocalDate.now().plusDays(7));
+            roomSearchFormDTO.setEndDate(LocalDate.now().plusDays(14));
+            List<HotelWithReservableRoomsDTO> hotelsWithReservableRoomsDTOS = hotelsWithReservableRoomsDTOTransformer.transformToHotelsWithReservableRoomsDTOs(roomService.searchHotelsWithReservableRooms(roomSearchDTOTransformer.transformToRoomSearchFormServiceDTO(roomSearchFormDTO)));
+            request.getSession().setAttribute("resultDTOS", hotelsWithReservableRoomsDTOS);
+        }
 
         return "newhomepage";
     }
