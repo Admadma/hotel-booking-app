9985b3bccd618c5c47d2a563e4ce92cf3d52c47c
Admadma <admadmaa77@gmail.com>
2024-04-16 22:32:44 +0200

    Added test for HotelServiceImpl.updateAverageRating

--- hotel-booking-app-service/src/test/java/com/application/hotelbooking/services/HotelServiceImplTest.java
+++ hotel-booking-app-service/src/test/java/com/application/hotelbooking/services/HotelServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.application.hotelbooking.services;
 
 import com.application.hotelbooking.domain.HotelModel;
+import com.application.hotelbooking.domain.ReviewModel;
 import com.application.hotelbooking.domain.RoomModel;
 import com.application.hotelbooking.dto.HotelCreationServiceDTO;
 import com.application.hotelbooking.exceptions.InvalidHotelException;
@@ -23,12 +24,16 @@ public class HotelServiceImplTest {
 
     public static final long HOTEL_ID = 1l;
     public static final String HOTEL_NAME = "Existing hotel";
-    public static final HotelModel HOTEL_MODEL = HotelModel.builder().rooms(List.of()).build();
+    public static final ReviewModel REVIEW_MODEL_ONE = ReviewModel.builder().rating(2).build();
+    public static final ReviewModel REVIEW_MODEL_TWO = ReviewModel.builder().rating(5).build();
+    public static final HotelModel HOTEL_MODEL = HotelModel.builder().rooms(List.of()).averageRating(0.0).reviews(List.of(REVIEW_MODEL_ONE, REVIEW_MODEL_TWO)).build();
+    public static final HotelModel HOTEL_MODEL_WITH_UPDATED_AVERAGE_REVIEWS = HotelModel.builder().rooms(List.of()).averageRating(3.5).reviews(List.of(REVIEW_MODEL_ONE, REVIEW_MODEL_TWO)).build();
     public static final HotelModel HOTEL_MODEL_WITH_ROOMS = HotelModel.builder().rooms(List.of(RoomModel.builder().roomNumber(1).build())).build();
     public static final HotelCreationServiceDTO HOTEL_CREATION_SERVICE_DTO = HotelCreationServiceDTO.builder().hotelName(HOTEL_NAME).build();
     public static final Optional<HotelModel> OPTIONAL_HOTEL_MODEL = Optional.of(HOTEL_MODEL);
     public static final Optional<HotelModel> OPTIONAL_HOTEL_MODEL_WITH_ROOMS = Optional.of(HOTEL_MODEL_WITH_ROOMS);
     public static final Optional<HotelModel> EMPTY_HOTEL_MODEL = Optional.empty();
+
     @InjectMocks
     private HotelServiceImpl hotelService;
 
@@ -74,5 +79,14 @@ public class HotelServiceImplTest {
 
         verify(hotelRepositoryService).getHotelById(HOTEL_ID);
         Assertions.assertThat(latestRoomNumberOfHotel).isEqualTo(0);
+    }
+
+    @Test
+    public void testUpdateAverageRatingShould(){
+        when(hotelRepositoryService.save(HOTEL_MODEL_WITH_UPDATED_AVERAGE_REVIEWS)).thenReturn(HOTEL_MODEL_WITH_UPDATED_AVERAGE_REVIEWS);
+
+        hotelService.updateAverageRating(HOTEL_MODEL);
+
+        verify(hotelRepositoryService).save(HOTEL_MODEL_WITH_UPDATED_AVERAGE_REVIEWS);
     }
 }
