5b3356b5d57953080da85f93a84dff0fad0e4a20
Admadma <admadmaa77@gmail.com>
2024-04-17 22:25:34 +0200

    Fixed ReserveRoomController tests

--- hotel-booking-app-web/src/test/java/com/application/hotelbooking/controllers/ReserveRoomControllerTest.java
+++ hotel-booking-app-web/src/test/java/com/application/hotelbooking/controllers/ReserveRoomControllerTest.java
@@ -7,13 +7,12 @@ import com.application.hotelbooking.security.SecurityConfiguration;
 import com.application.hotelbooking.services.ReservationService;
 import com.application.hotelbooking.transformers.HotelsWithReservableRoomsDTOTransformer;
 import com.application.hotelbooking.transformers.ReservationPlanTransformer;
-import com.application.hotelbooking.transformers.ReservationViewTransformer;
-import com.application.hotelbooking.transformers.RoomSearchDTOTransformer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -33,26 +32,6 @@ public class ReserveRoomControllerTest {
     private static final int ROOM_NUMBER = 1;
     private static final LocalDate START_DATE = LocalDate.of(2024, 3, 1);
     private static final LocalDate END_DATE = LocalDate.of(2024, 3, 5);
-    private static final ReservableRoomViewDTO RESERVABLE_ROOM_VIEW_DTO = new ReservableRoomViewDTO();
-    private static final ReservableRoomDTO RESERVABLE_ROOM_DTO = new ReservableRoomDTO();
-    private static final List<ReservableRoomViewDTO> RESERVABLE_ROOM_VIEW_DTO_LIST = List.of(RESERVABLE_ROOM_VIEW_DTO);
-    private static final HotelView HOTEL_VIEW = HotelView.builder()
-            .hotelName("Test Hotel")
-            .city("Test City")
-            .build();
-    private static final RoomView ROOM_VIEW = RoomView.builder()
-            .roomNumber(1)
-            .hotel(HOTEL_VIEW)
-            .roomType(RoomType.FAMILY_ROOM)
-            .singleBeds(1)
-            .doubleBeds(1)
-            .build();
-    private static final ReservationView RESERVATION_VIEW = ReservationView.builder()
-            .room(ROOM_VIEW)
-            .startDate(LocalDate.now().plusDays(5))
-            .endDate(LocalDate.now().plusDays(10))
-            .totalPrice(100)
-            .build();
     private static final String USER_NAME = "testUser";
     private static final ReservationModel RESERVATION_MODEL = new ReservationModel();
 
@@ -83,9 +62,7 @@ public class ReserveRoomControllerTest {
             .uniqueReservableRoomOfHotelServiceDTOList(List.of(UNIQUE_RESERVABLE_ROOM_OF_HOTEL_SERVICE_DTO))
             .build();
     private static List<HotelWithReservableRoomsDTO> HOTEL_WITH_RESERVABLE_ROOMS_DTO_LIST = List.of(HOTEL_WITH_RESERVABLE_ROOMS_DTO);
-    private static List<HotelWithReservableRoomsDTO> EMPTY_HOTEL_WITH_RESERVABLE_ROOMS_DTO_LIST = List.of();
     private static List<HotelWithReservableRoomsServiceDTO> HOTEL_WITH_RESERVABLE_ROOMS_SERVICE_DTO_LIST = List.of(HOTEL_WITH_RESERVABLE_ROOMS_SERVICE_DTO);
-    private static List<HotelWithReservableRoomsServiceDTO> EMPTY_HOTEL_WITH_RESERVABLE_ROOMS_SERVICE_DTO_LIST = List.of();
     private static final ReservationPlanServiceDTO RESERVATION_PLAN_SERVICE_DTO = ReservationPlanServiceDTO.builder()
             .hotelName(HOTEL_NAME)
             .startDate(START_DATE)
@@ -97,13 +74,8 @@ public class ReserveRoomControllerTest {
             .endDate(END_DATE)
             .build();
 
-
     @MockBean
     private ReservationService reservationService;
-    @MockBean
-    private RoomSearchDTOTransformer roomSearchDTOTransformer;
-    @MockBean
-    private ReservationViewTransformer reservationViewTransformer;
     @MockBean
     private HotelsWithReservableRoomsDTOTransformer hotelsWithReservableRoomsDTOTransformer;
     @MockBean
@@ -146,7 +118,7 @@ public class ReserveRoomControllerTest {
     }
 
     @Test
-    @WithMockUser(authorities = "ADMIN")
+    @WithMockUser(authorities = "ADMIN", username = "admin")
     public void testAdminUserCanNotAttemptToReserveRoom() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
                         .post("/hotelbooking/reserve")
@@ -154,55 +126,55 @@ public class ReserveRoomControllerTest {
                 .andExpect(status().isForbidden())
                 .andExpect(request().sessionAttribute("reservationPlan", RESERVATION_PLAN_DTO));
     }
-//
-//    @Test
-//    @WithMockUser(authorities = "USER", username = USER_NAME)
-//    public void testReserveShouldRedirectToHomePageWithReservationErrorAndClearSessionAttributesIfOutdatedReservationExceptionWasThrown() throws Exception {
-//        when(reservationViewTransformer.transformToReservationModel(RESERVATION_VIEW)).thenReturn(RESERVATION_MODEL);
-//        when(reservationService.reserveRoom(RESERVATION_PLAN_SERVICE_DTO, USER_NAME)).thenThrow(OutdatedReservationException.class);
-//
-//        mockMvc.perform(MockMvcRequestBuilders
-//                        .post("/hotelbooking/reserve")
-//                        .sessionAttr("reservationPlan", RESERVATION_VIEW))
-//                .andExpect(redirectedUrl("/hotelbooking/home?reservationError"))
-//                .andExpect(request().sessionAttributeDoesNotExist("reservationPlan"))
-//                .andExpect(request().sessionAttributeDoesNotExist("resultDTOS"));
-//
-//        verify(reservationViewTransformer).transformToReservationModel(RESERVATION_VIEW);
-//        verify(reservationService).reserveRoom(RESERVATION_MODEL);
-//    }
 
-//    @Test
-//    @WithMockUser(authorities = "USER")
-//    public void testReserveShouldRedirectToHomePageWithReservationErrorAndClearSessionAttributesIfAnyOtherExceptionWasThrown() throws Exception {
-//        when(reservationViewTransformer.transformToReservationModel(RESERVATION_VIEW)).thenReturn(RESERVATION_MODEL);
-//        when(reservationService.reserveRoom(RESERVATION_MODEL)).thenThrow(DataIntegrityViolationException.class);
-//
-//        mockMvc.perform(MockMvcRequestBuilders
-//                        .post("/hotelbooking/reserve")
-//                        .sessionAttr("reservationPlan", RESERVATION_VIEW))
-//                .andExpect(redirectedUrl("/hotelbooking/home?reservationError"))
-//                .andExpect(request().sessionAttributeDoesNotExist("reservationPlan"))
-//                .andExpect(request().sessionAttributeDoesNotExist("resultDTOS"));
-//
-//        verify(reservationViewTransformer).transformToReservationModel(RESERVATION_VIEW);
-//        verify(reservationService).reserveRoom(RESERVATION_MODEL);
-//    }
-//
-//    @Test
-//    @WithMockUser(authorities = "USER")
-//    public void testReserveShouldRedirectToMyReservationsPageWithReservationSuccessAndClearSessionAttributesIfRoomSuccessfullyReserved() throws Exception {
-//        when(reservationViewTransformer.transformToReservationModel(RESERVATION_VIEW)).thenReturn(RESERVATION_MODEL);
-//        when(reservationService.reserveRoom(RESERVATION_MODEL)).thenReturn(RESERVATION_MODEL);
-//
-//        mockMvc.perform(MockMvcRequestBuilders
-//                        .post("/hotelbooking/reserve")
-//                        .sessionAttr("reservationPlan", RESERVATION_VIEW))
-//                .andExpect(redirectedUrl("/hotelbooking/myreservations?reservationSuccess"))
-//                .andExpect(request().sessionAttributeDoesNotExist("reservationPlan"))
-//                .andExpect(request().sessionAttributeDoesNotExist("resultDTOS"));
-//
-//        verify(reservationViewTransformer).transformToReservationModel(RESERVATION_VIEW);
-//        verify(reservationService).reserveRoom(RESERVATION_MODEL);
-//    }
+    @Test
+    @WithMockUser(authorities = "USER", username = USER_NAME)
+    public void testReserveShouldRedirectToHomePageWithReservationErrorAndClearSessionAttributesIfOutdatedReservationExceptionWasThrown() throws Exception {
+        when(reservationPlanTransformer.transformToReservationPlanServiceDTO(RESERVATION_PLAN_DTO)).thenReturn(RESERVATION_PLAN_SERVICE_DTO);
+        when(reservationService.reserveRoom(RESERVATION_PLAN_SERVICE_DTO, USER_NAME)).thenThrow(OutdatedReservationException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/hotelbooking/reserve")
+                        .sessionAttr("reservationPlan", RESERVATION_PLAN_DTO))
+                .andExpect(redirectedUrl("/hotelbooking/home?reservationError"))
+                .andExpect(request().sessionAttributeDoesNotExist("reservationPlan"))
+                .andExpect(request().sessionAttributeDoesNotExist("hotelsRoomsResultDTOs"));
+
+        verify(reservationPlanTransformer).transformToReservationPlanServiceDTO(RESERVATION_PLAN_DTO);
+        verify(reservationService).reserveRoom(RESERVATION_PLAN_SERVICE_DTO, USER_NAME);
+    }
+
+    @Test
+    @WithMockUser(authorities = "USER", username = USER_NAME)
+    public void testReserveShouldRedirectToHomePageWithReservationErrorAndClearSessionAttributesIfAnyOtherExceptionWasThrown() throws Exception {
+        when(reservationPlanTransformer.transformToReservationPlanServiceDTO(RESERVATION_PLAN_DTO)).thenReturn(RESERVATION_PLAN_SERVICE_DTO);
+        when(reservationService.reserveRoom(RESERVATION_PLAN_SERVICE_DTO, USER_NAME)).thenThrow(DataIntegrityViolationException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/hotelbooking/reserve")
+                        .sessionAttr("reservationPlan", RESERVATION_PLAN_DTO))
+                .andExpect(redirectedUrl("/hotelbooking/home?reservationError"))
+                .andExpect(request().sessionAttributeDoesNotExist("reservationPlan"))
+                .andExpect(request().sessionAttributeDoesNotExist("resultDTOS"));
+
+        verify(reservationPlanTransformer).transformToReservationPlanServiceDTO(RESERVATION_PLAN_DTO);
+        verify(reservationService).reserveRoom(RESERVATION_PLAN_SERVICE_DTO, USER_NAME);
+    }
+
+    @Test
+    @WithMockUser(authorities = "USER", username = USER_NAME)
+    public void testReserveShouldRedirectToMyReservationsPageWithReservationSuccessAndClearSessionAttributesIfRoomSuccessfullyReserved() throws Exception {
+        when(reservationPlanTransformer.transformToReservationPlanServiceDTO(RESERVATION_PLAN_DTO)).thenReturn(RESERVATION_PLAN_SERVICE_DTO);
+        when(reservationService.reserveRoom(RESERVATION_PLAN_SERVICE_DTO, USER_NAME)).thenReturn(RESERVATION_MODEL);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/hotelbooking/reserve")
+                        .sessionAttr("reservationPlan", RESERVATION_PLAN_DTO))
+                .andExpect(redirectedUrl("/hotelbooking/myreservations?reservationSuccess"))
+                .andExpect(request().sessionAttributeDoesNotExist("reservationPlan"))
+                .andExpect(request().sessionAttributeDoesNotExist("resultDTOS"));
+
+        verify(reservationPlanTransformer).transformToReservationPlanServiceDTO(RESERVATION_PLAN_DTO);
+        verify(reservationService).reserveRoom(RESERVATION_PLAN_SERVICE_DTO, USER_NAME);
+    }
 }
