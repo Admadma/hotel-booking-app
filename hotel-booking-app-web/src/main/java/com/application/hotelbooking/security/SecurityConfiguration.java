103881db2beed2d85ad0ebfc444935318e047cb8
Admadma <admadmaa77@gmail.com>
2024-04-18 23:00:20 +0200

    Sign up page can only be accessed by unauthenticated users

--- hotel-booking-app-web/src/main/java/com/application/hotelbooking/security/SecurityConfiguration.java
+++ hotel-booking-app-web/src/main/java/com/application/hotelbooking/security/SecurityConfiguration.java
@@ -21,7 +21,8 @@ public class SecurityConfiguration {
                     auth.requestMatchers("/hotelbooking/admin/**").hasAnyAuthority("ADMIN");
                     auth.requestMatchers("/hotelbooking/reserve-room/**", "/hotelbooking/my-reservations/**", "/hotelbooking/review/**").hasAnyAuthority("USER");
                     auth.requestMatchers("/hotelbooking/default/**", "/hotelbooking/account/**").authenticated();
-                    auth.requestMatchers("/hotelbooking/login/**", "/hotelbooking/register/**", "/error/**", "/images/**", "/hotelbooking/home/**").permitAll();
+                    auth.requestMatchers("/hotelbooking/login/**", "/error/**", "/images/**", "/hotelbooking/home/**").permitAll();
+                    auth.requestMatchers("/hotelbooking/register/**").anonymous();
                 })
                 .formLogin(form -> form
                         .loginPage("/hotelbooking/login")
