6e40cdd8933f5a45b6c2bd4bdf9da38c74ec9ca3
Admadma <admadmaa77@gmail.com>
2024-04-19 12:52:29 +0200

    Updated Review page UI

--- hotel-booking-app-service/src/main/java/com/application/hotelbooking/wrappers/implementations/UUIDWrapperImpl.java
+++ hotel-booking-app-service/src/main/java/com/application/hotelbooking/wrappers/implementations/UUIDWrapperImpl.java
@@ -1,16 +1,21 @@
 package com.application.hotelbooking.wrappers.implementations;
 
 import com.application.hotelbooking.wrappers.UUIDWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
 public class UUIDWrapperImpl implements UUIDWrapper {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UUIDWrapper.class);
+
     @Override
     public UUID getRandomUUID() {
         UUID uuid = UUID.randomUUID();
-        System.out.println("The generated UUID: " + uuid);
+        LOGGER.info("The generated UUID: " + uuid);
         return uuid;
     }
 }
