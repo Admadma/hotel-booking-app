a7e68438ea9b33ee8b5686a64e2c191b20c56949
Admadma <admadmaa77@gmail.com>
2024-04-23 13:47:09 +0200

    Removed unused labels from views.RoomType

--- hotel-booking-app-web/src/main/java/com/application/hotelbooking/views/RoomType.java
+++ hotel-booking-app-web/src/main/java/com/application/hotelbooking/views/RoomType.java
@@ -1,16 +1,9 @@
 package com.application.hotelbooking.views;
 
 public enum RoomType {
-    FAMILY_ROOM("Family Room"),
-    SINGLE_ROOM("Single Room"),
-    DOUBLE_ROOM("Double Room"),
-    TWIN_ROOM("Twin Room"),
-    SUITE_ROOM("Suite Room");
-
-    public final String label;
-
-    private RoomType(String label){
-        this.label=label;
-    }
-
+    FAMILY_ROOM,
+    SINGLE_ROOM,
+    DOUBLE_ROOM,
+    TWIN_ROOM,
+    SUITE_ROOM;
 }
