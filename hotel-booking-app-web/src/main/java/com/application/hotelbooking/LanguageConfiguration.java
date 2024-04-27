29ec7350ae4bbfa544bb803fe3f977dec13a5ba9
Admadma <admadmaa77@gmail.com>
2024-04-28 01:39:56 +0200

    Changed default language from en to hu

--- hotel-booking-app-web/src/main/java/com/application/hotelbooking/LanguageConfiguration.java
+++ hotel-booking-app-web/src/main/java/com/application/hotelbooking/LanguageConfiguration.java
@@ -26,7 +26,7 @@ public class LanguageConfiguration implements WebMvcConfigurer {
     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.US);
+        slr.setDefaultLocale(new Locale("hu"));
         return slr;
     }
 
